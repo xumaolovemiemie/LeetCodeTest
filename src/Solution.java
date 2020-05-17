e152f4d56835d0e12bd007277aa39a6abade3008
sunjie <13761719246@163.com>
2020-05-17 22:39:05 +0800

    算法题解练习 11.盛水最多的容器 70.爬楼梯

---
+++ src/Solution.java
@@ -0,0 +1,58 @@
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * 11. 盛最多水的容器
+     * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+     * <p>
+     * 说明：你不能倾斜容器，且 n 的值至少为 2。
+     */
+    public static int maxArea(int[] height) {
+        int max = 0;
+        for (int i = 0, j = height.length - 1; i < j; ) {
+            int minHeight = height[i] < height[j] ? height[i++] : height[j--];
+            max = Math.max(max, (j - i) * minHeight);
+        }
+        return max;
+    }
+
+    /**
+     * 15. 三数之和
+     * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+     * <p>
+     * 注意：答案中不可以包含重复的三元组。
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        return null;
+    }
+
+    /**
+     * 70. 爬楼梯
+     * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+     * <p>
+     * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+     * <p>
+     * 注意：给定 n 是一个正整数。
+     */
+    public static int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int num = 0;
+        int num_1 = 2;
+        int num_2 = 1;
+        for (int i = 3; i <= n; i++) {
+            num = num_1 + num_2;
+            num_2 = num_1;
+            num_1 = num;
+        }
+        return num;
+    }
+}
