abb43613b62a41c06e34ee519ba14b1d2ab8c404
sunjie3 <sunjie3@datangmobile.cn>
2020-05-18 12:38:03 +0800

    66.加一

--- src/Solution.java
+++ src/Solution.java
@@ -70,6 +70,28 @@ public class Solution {
     }
 
     /**
+     * 66.加一
+     * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+     * <p>
+     * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+     * <p>
+     * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+     */
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i] += 1;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        int[] ints = new int[digits.length + 1];
+        ints[0] = 1;
+        return ints;
+    }
+
+    /**
      * 70. 爬楼梯
      * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
      * <p>
