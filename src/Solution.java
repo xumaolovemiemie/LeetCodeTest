f24a151e2f5e87404930cfb48c362d32c6321a90
sunjie <13761719246@163.com>
2020-05-20 23:26:47 +0800

    641. 设计循环双端队列

--- src/Solution.java
+++ src/Solution.java
@@ -333,4 +333,92 @@ public class Solution {
     public static int fib(int N) {
 
     }
+
+    /**
+     * 641. 设计循环双端队列
+     * <p>
+     * 设计实现双端队列。
+     * 你的实现需要支持以下操作：
+     * <p>
+     * MyCircularDeque(k)：构造函数,双端队列的大小为k。
+     * insertFront()：将一个元素添加到双端队列头部。 如果操作成功返回 true。
+     * insertLast()：将一个元素添加到双端队列尾部。如果操作成功返回 true。
+     * deleteFront()：从双端队列头部删除一个元素。 如果操作成功返回 true。
+     * deleteLast()：从双端队列尾部删除一个元素。如果操作成功返回 true。
+     * getFront()：从双端队列头部获得一个元素。如果双端队列为空，返回 -1。
+     * getRear()：获得双端队列的最后一个元素。 如果双端队列为空，返回 -1。
+     * isEmpty()：检查双端队列是否为空。
+     * isFull()：检查双端队列是否满了。
+     * <p>
+     * 提示：
+     * <p>
+     * 所有值的范围为 [1, 1000]
+     * 操作次数的范围为 [1, 1000]
+     * 请不要使用内置的双端队列库。
+     */
+    public static class MyCircularDeque {
+
+        /**
+         * Initialize your data structure here. Set the size of the deque to be k.
+         */
+        public MyCircularDeque(int k) {
+
+        }
+
+        /**
+         * Adds an item at the front of Deque. Return true if the operation is successful.
+         */
+        public boolean insertFront(int value) {
+
+        }
+
+        /**
+         * Adds an item at the rear of Deque. Return true if the operation is successful.
+         */
+        public boolean insertLast(int value) {
+
+        }
+
+        /**
+         * Deletes an item from the front of Deque. Return true if the operation is successful.
+         */
+        public boolean deleteFront() {
+
+        }
+
+        /**
+         * Deletes an item from the rear of Deque. Return true if the operation is successful.
+         */
+        public boolean deleteLast() {
+
+        }
+
+        /**
+         * Get the front item from the deque.
+         */
+        public int getFront() {
+
+        }
+
+        /**
+         * Get the last item from the deque.
+         */
+        public int getRear() {
+
+        }
+
+        /**
+         * Checks whether the circular deque is empty or not.
+         */
+        public boolean isEmpty() {
+
+        }
+
+        /**
+         * Checks whether the circular deque is full or not.
+         */
+        public boolean isFull() {
+
+        }
+    }
 }
