a369bf8f1a6b7e6a3ec297f581eca0de1d9eb475
sunjie <13761719246@163.com>
2020-05-20 21:56:13 +0800

    20.有效的括号

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,4 @@
+import javax.print.DocFlavor;
 import java.util.*;
 
 public class Solution {
@@ -84,6 +85,36 @@ public class Solution {
             }
         }
         return lists;
+    }
+
+    /**
+     * 20. 有效的括号
+     * <p>
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+     * <p>
+     * 有效字符串需满足：
+     * <p>
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     * 注意空字符串可被认为是有效字符串。
+     */
+    public static boolean isValid(String s) {
+        Map<Character, Character> map = new TreeMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (char aChar : chars) {
+            if (map.containsKey(aChar)) {
+                if (stack.size() == 0 || !map.get(aChar).equals(stack.pop())) {
+                    return false;
+                }
+            } else {
+                stack.push(aChar);
+            }
+        }
+        return stack.size() == 0;
     }
 
     /**
